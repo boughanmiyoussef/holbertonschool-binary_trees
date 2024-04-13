5bffe3a4e58c49d7ba60da033e884c9f4ee5e175
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-13 19:47:47 +0100

    binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_insert_left - function name
  * @parent: parent
- * @value: value 
+ * @value: value
  * Return: NULL if it fails or the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
